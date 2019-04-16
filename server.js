c0253e11e90328dfd678b0e0aba7a4eecf2ec128
illia <illia.ohrimenko@gmail.com>
2019-04-16 17:48:59 +0300

    Add test images and update server

--- server.js
+++ server.js
@@ -1,33 +1,63 @@
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const Telegraf = require('telegraf');
+const config = require('config');
+const images = require('./routes/images');
+const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
 
-var images = require("./routes/images");
+const port = config.get('port');
 
-const cors = require("cors");
+const BOT_TOKEN = config.get('token');
+const bot = new Telegraf(BOT_TOKEN);
 
-var port = 5000;
+//Telegram bot
+bot.start(ctx => ctx.reply('Hi! Send me images.'));
+bot.on('photo', ctx => {
+  imageUrl = ctx.telegram
+    .getFileLink(ctx.message.photo[ctx.message.photo.length - 1].file_id)
+    .then(function(fileData) {
+      const downloadUrl = fileData;
+      const dateNow = new Date();
+      const fileName =
+        './uploads/' +
+        dateNow.toDateString() +
+        dateNow.toLocaleTimeString('en-GB') +
+        '.jpg';
+      const file = fs.createWriteStream(fileName);
+      const request = https.get(downloadUrl, function(response) {
+        response.pipe(file);
+      });
+      console.log('Image saved to upload directory');
+    });
+  ctx.reply('Image saved');
+});
+bot.launch();
 
-var app = express();
-
+const app = express();
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true
-    })
+  cors({
+    origin: config.get('corsUrl'),
+    credentials: true
+  })
 );
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.engine("html", require("ejs").renderFile);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
-app.use(express.static(path.join(__dirname, "client")));
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, '/uploads')));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
-app.use("/api", images);
+app.use('/api', images);
 
 app.listen(port, function() {
-    console.log("Server started on port " + port);
+  console.log('Server started on port ' + port);
 });
