06af441054c304b963d0d89f2a5baf8b664b4620
illia <illia.ohrimenko@gmail.com>
2019-04-16 17:48:59 +0300

    Add test images and update server

--- routes/images.js
+++ routes/images.js
@@ -1,39 +1,46 @@
-var express = require("express");
-var router = express.Router();
-var multerConfig = require("../config/multer");
-var app = express();
+const express = require("express");
+const router = express.Router();
+const multerConfig = require("../config/multer");
+const fs = require("fs");
 
 
 router.get ("/get", (req, res) => {
-    console.log('get image');
     res.send(_getAllFilesFromFolder("./uploads"));
 });
 
 router.post("/images", multerConfig.saveToUploads, (req, res) => {
-    return res.json(
-        {
-            "messege": "file uploaded successfully"
+    if (!req.file) {
+         return res.status(400).json({
+            success: false,
+            "msg": "No file uploaded"
+        })
+
+    } else {
+        return res.json({
+            success: true,
+            "msg": "File uploaded successfully"
         });
+    }
 });
 
 var _getAllFilesFromFolder = function(dir) {
 
-    var filesystem = require("fs");
     var results = [];
 
-    filesystem.readdirSync(dir).forEach(function(file) {
+    fs.readdirSync(dir).forEach(function(file) {
 
-        file = dir+'/'+file;
-        var stat = filesystem.statSync(file);
+       // file = dir+'/'+file;
+        var stat = fs.statSync(dir+'/'+file);
 
         if (stat && stat.isDirectory()) {
             results = results.concat(_getAllFilesFromFolder(file))
-        } else results.push(file);
+        } else results.push("http://localhost:5000/" + file);
 
     });
 
     return results;
 
 };
+
 
 module.exports = router;
